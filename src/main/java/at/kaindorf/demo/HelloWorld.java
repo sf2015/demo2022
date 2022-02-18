99e0f81ebb20c6c0f5cadf813316ffd34f46fdce
heinz <sf@htl-kaindorf.ac.at>
2022-02-18 09:33:30 +0100

    First commit

---
+++ src/main/java/at/kaindorf/demo/HelloWorld.java
@@ -0,0 +1,13 @@
+package at.kaindorf.demo;
+
+/**
+ * Project: Demo3DHIF
+ * Created by: SF
+ * Date: 18.02.2022
+ * Time: 09:31
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
